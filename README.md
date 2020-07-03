8f88d8b6281308762aaa808b9fecf91b7f1f7c2e
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2020-07-03 14:22:04 -0400

    added simple server-side functionality to get spotify playlists

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# TheGoodVibes
-Hackathon/Hacation Project
-----------------------------------------------------------------------------------
+# TheGoodVibes - Hackathon/Hacation Project
 A website that uses a Spotify API to give you relaxing music to vibe to.
+
+# Install
+To run this website locally make sure you have the following installed:
+- [NodeJS](https://nodejs.org/en/)
+
+Make sure you have spotify developer account with a Client ID and Client Secret
+- rename `sample.env` to `.env` and input your tokens 
+
+### Setup:
+1. Open a terminal in the project's root directory 
+2. Run `npm i` to install the packages used in this project
+3. Run `npm run start` to run the server
+4. Open a browser and visit http://localhost:3000
+
+# Contributors:
+* [JuliaYu2002](https://github.com/JuliaYu2002)
+* [RafaelPiloto10](https://github.com/RafaelPiloto10)
+* [carolynqu](https://github.com/carolynqu)
+* [vyshnavirajeevan](https://github.com/vyshnavirajeevan)
+* [elisaj1317](https://github.com/elisaj1317)
+* [AngelynDisguise](https://github.com/AngelynDisguise)
--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const app = express();
+const Spotify = require('node-spotify-api');
+require('dotenv').config();
 const port = process.env.PORT || 3000;
+
+const spotify = new Spotify({
+    id: process.env.CLIENT_ID,
+    secret: process.env.CLIENT_SECRET
+});
+
+const playlists = {
+    moodbooster: "37i9dQZF1DX3rxVfibe1L0"
+}
 
 app.use(express.static("public/"));
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log("Server is up and running");
+
+    // Simple demo that gets the 'mood booster' playlist from spotify
+
+    let playlist = await getPlaylist(playlists['moodbooster']);
+    for (let song of playlist) {
+        console.log(song);
+    }
 });
+
+
+const getPlaylist = async (id) => {
+    return new Promise((resolve, reject) => {
+        spotify
+            .request(`https://api.spotify.com/v1/playlists/${id}/tracks`)
+            .then(function (playlist) {
+                resolve(playlist.items);
+            })
+            .catch(function (err) {
+                console.error('Error occurred: ' + err);
+                reject(err);
+            });
+    });
+
+}
