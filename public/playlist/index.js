5896881ef179e2f80bf63874fbc3db90a442eab3
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2020-07-04 20:23:04 -0400

    fixed getting playlist bug in playlists page

--- public/playlist/index.js
+++ public/playlist/index.js
@@ -1,6 +1,6 @@
 window.onload = async () => {
     // use this link as a test for now: http://localhost:3000/playlist/?playlists=1kzqdyFOm4PSBheh6Br3Nz,6U86PcQkkVFpjiElUVoz0c,2QwcMBmizP3fHUuEao1W07
-    const playlists = new URLSearchParams(window.location.search).split(",");
+    const playlists = new URLSearchParams(window.location.search).get('playlists').split(",");
     // Iterate through each array
     //     get the playlist data using await fetch("/get-playlist/ playlist_id ")
     //     Display the playlist information
