0ca7486f0b62d25570c9fff4696e556782f3df46
elisaj1317 <41582626+elisaj1317@users.noreply.github.com>
2020-07-05 02:08:14 -0500

    added song names

--- public/playlist/index.js
+++ public/playlist/index.js
@@ -14,6 +14,7 @@ window.onload = async () => {
             let playlist_id = playlists[i];
             fetch("/get-playlist/" + playlist_id).then(data => data.json()).then(playlist_data => {
                 console.log(playlist_data);
+                console.log(playlist_data.tracks.items[0].track.name)
 
                 let card = document.createElement("DIV");
                 card.classList.add("card", "bg-light", "justify-content-center", "mx-auto");
@@ -28,23 +29,40 @@ window.onload = async () => {
 
                 let p = document.createElement("P");
                 p.classList.add("card-text", "playlistName", "text-center");
-                p.innerText = playlist_data.name
+                p.innerText = playlist_data.name;
 
 
                 card_body.appendChild(img);
                 card_body.appendChild(p);
 
+                let p2 = document.createElement("P");
+                p2.classList.add("card-text", "playlistName", "listFormat", "listTitle");
+                p2.innerText = "Top songs:";
+                card_body.appendChild(p2);
+
+
+                let ol = document.createElement("OL");
+                ol.classList.add("listFormat");
+                card_body.appendChild(ol);
+
+                for (let j = 0; j < 3; j++) {
+
+                  let li = document.createElement("LI");
+                  li.innerText = playlist_data.tracks.items[j].track.name;
+                  ol.appendChild(li);
+
+                }
+
                 if ("spotify" in playlist_data.external_urls) {
 
                   let a = document.createElement("A");
-                  a.classList.add("spotifyLinks", "text-center")
+                  a.classList.add("spotifyLinks", "text-center");
                   a.href = playlist_data.external_urls['spotify'];
                   a.innerText = "Click here to listen to the playlist!";
 
                   card_body.appendChild(a);
 
                 }
-                
 
                 card.appendChild(card_body);
                 document.getElementById("suggested-playlists").appendChild(card);
